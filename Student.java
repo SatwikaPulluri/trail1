8c905e6efd4929c15266e28a02c93d73bb084eb7
SatwikaPulluri <118365188+SatwikaPulluri@users.noreply.github.com>
2025-06-08 14:38:57 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public static void main(String[] args) {
+        String name = "Satvika";
+        int rno = 123;
+        String dept = "AIML";
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rno);
+        System.out.println("Dept: " + dept);
+    }
+}
